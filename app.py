14040e9ab247765a47aed72786a20106da3de3ef
Nan Wang <nan.wang@jina.ai>
2021-10-16 20:23:55 +0800

    feat: switch to psql

--- app.py
+++ app.py
@@ -12,31 +12,17 @@ def index():
             id='zvXkQkqd2I8',
             uri=str((Path(__file__).parent / 'toy-data' / 'zvXkQkqd2I8.vtt').absolute()))
         docs.append(doc)
-        resp = f.post(on='/index', inputs=docs, return_results=True)
-        f.post(on='/dump', parameters={'dump_path': './workspace/dump_lmdb', 'shards': 1})
-        print(f'docs: {len(docs)}')
-        for d in resp[0].docs:
-            print(f'chunks: {len(d.chunks)}')
-            for c in d.chunks:
-                print(f'{c.id}: {c.text}')
-
-
-def query():
-    f = Flow.load_config('flows/query.yml',
-                         override_with={
-                             'protocol': 'grpc',
-                             'cors': False})
-    with f:
-        resp = f.post(on='/search', inputs=[Document(text='is jina a one-liner?')], return_results=True)
-        for d in resp[0].docs:
-            for m in d.matches:
-                print(f'{d.id}: {m.id}, {m.scores["relevance"]}, {m.location}, {m.text}, {m.tags}')
+        f.post(on='/index', inputs=docs)
 
 
 def query_restful():
     f = Flow.load_config('flows/query.yml')
 
     with f:
+        # create a psql snapshot
+        f.post(on='/snapshot')
+        # load data into faiss
+        f.post(on='/sync', parameters={'only_delta': True})
         f.block()
 
 
